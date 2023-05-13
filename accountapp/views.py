ae8f2ef3d3107809fdc121750634d7b6ad2223ce
김의찬 <yuichan99@naver.com>
2023-05-13 17:11:15 +0900

    django course 26 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse, reverse_lazy
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 
 # Create your views here.
@@ -26,14 +26,14 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
-    model = User
-    form_class = UserCreationForm
+    model = User  #장고에서는 기본적으로 제공하는 model 따로 지정 x
+    form_class = UserCreationForm  #이것도 장고에서 기본적으로 제공
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView):
     model = User
-    context_object_name = 'target_user'
+    context_object_name = 'target_user'   #user 모델의 별명 느낌.
     template_name ='accountapp/detail.html'
 
 class AccountUpdateView(UpdateView):
@@ -41,3 +41,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
