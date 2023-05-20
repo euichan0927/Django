79370765b5e4f2e4d9633c11f78c4deb8cd299ad
김의찬 <yuichan99@naver.com>
2023-05-20 22:59:21 +0900

    django course 32 commit

--- euichan/urls.py
+++ euichan/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path , include
+
+from euichan import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
