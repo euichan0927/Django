fa8a2c3ea61a1e965a3b920770f037ba7c64336b
김의찬 <yuichan99@naver.com>
2023-05-23 22:08:48 +0900

    django course 35 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,10 +10,12 @@ class Project:
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article',null=True)
+
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
 
     created_at = models.DateField(auto_now_add=True,null=True)
+
+
