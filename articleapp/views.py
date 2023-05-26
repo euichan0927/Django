070dbe9a8dc85ccaf6ae6b56ff82086efbfa8f35
김의찬 <yuichan99@naver.com>
2023-05-27 02:27:40 +0900

    django course 36 commit

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,11 +1,13 @@
 from audioop import reverse
+from msilib.schema import ListView
+
 from django.urls import reverse
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -27,7 +29,7 @@ class ArticleCreateView(CreateView):
         temp_article.save()
         return super().form_valid(form)
     def get_success_url(self):
-        return reverse_lazy('articleapp:detail',kwargs={'pk': self.object.pk})
+        return reverse('articleapp:detail',kwargs={'pk': self.object.pk})
 
 
 class ArticleDetailView(DetailView):
@@ -45,7 +47,7 @@ class ArticleUpdateView(UpdateView):
     template_name = 'articleapp/update.html'
 
     def get_success_url(self):
-        return reverse_lazy('articleapp:detail',kwargs={'pk': self.object.pk})
+        return reverse('articleapp:detail',kwargs={'pk': self.object.pk})
 
 
 
@@ -59,3 +61,11 @@ class ArticleDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse('articleapp:detail',kwargs={'pk': self.object.pk})
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 5
+
